ea1fd815093c67fe204b02b0da22f16d28c50994
Raynur <raynur.khasanov@gmail.com>
2019-04-16 19:03:43 +0300

    Верстка готова (нужно кое-где исправить)

--- AdaptTestProject/startpage/urls.py
+++ AdaptTestProject/startpage/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
 	path('', views.index, name='index-startpage'),
 	path('signup/', views.sign_up, name='signup-startpage'),
 	path('profile/', views.profile, name='profile-startpage'), 
-	path('test_creation_first/', views.create_test1, name='test_creation_first-startpage'),
-	path('test_creation_second/', views.create_test2, name='test_creation_second-startpage')
+	path('create_test1/', views.create_test1, name='test_creation_first-startpage'),
+	path('create_test2/', views.create_test2, name='test_creation_second-startpage'),
+	path('test_created_successfully/', views.test_created_successfully, name='test_created_successfully-startpage'),
+	path('my_created_tests/', views.my_created_tests, name="my_created_tests-startpage"), 
+	path('tests_page/', views.tests_page, name="tests_page-startpage"),
+	path('all_my_attempts/', views.all_my_attempts, name="all_my_attempts-startpage"), 
+	path('test_result/', views.test_result, name="test_result-startpage"), 
+	path('start_test/', views.start_test, name="start_test-startpage"), 
+	path('question/', views.question, name="question-startpage")
 ]
--- AdaptTestProject/startpage/views.py
+++ AdaptTestProject/startpage/views.py
@@ -18,4 +18,25 @@ def create_test1(request):
 
 #страница сооздания теста, где будут вноситься вопросы, варианты ответов и правильный ответы
 def create_test2(request):
-	return render(request,'startpage/test_creation_first.html')
+	return render(request,'startpage/test_creation_second.html')
+
+def test_created_successfully(request):
+	return render(request, 'startpage/test_created_successfully.html')
+
+def my_created_tests(request):
+	return render(request, 'startpage/my_created_tests.html')
+
+def tests_page(request):
+	return render(request, 'startpage/tests_page.html')
+
+def all_my_attempts(request):
+	return render(request, 'startpage/all_my_attempts.html')
+
+def test_result(request):
+	return render(request, 'startpage/test_result.html')
+
+def start_test(request):
+	return render(request, 'startpage/start_test.html')
+
+def question(request):
+	return render(request, 'startpage/question.html')
