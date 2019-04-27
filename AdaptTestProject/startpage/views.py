d21aea3d044a6feaca42741e5bdf30ac6f3a785c
Raynur <raynur.khasanov@gmail.com>
2019-04-28 00:47:31 +0300

    redirect поменял на reverse

--- AdaptTestProject/startpage/views.py
+++ AdaptTestProject/startpage/views.py
@@ -1,44 +1,30 @@
-
 import math
 import operator
 import random
-
-from django.shortcuts import render
-from django.shortcuts import render_to_response, redirect, get_object_or_404
-
 from django.contrib import auth
-from django.http import HttpResponseNotFound
-
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render, render_to_response, redirect
+from django.urls import reverse, reverse_lazy
 from django.template.context_processors import csrf
-
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
+
 from .forms import *
 from .models import *
 
 
 def index(request):
     return render(request, 'startpage/index.html')
-  
+
+@login_required(login_url=reverse_lazy('index'))
 def profile(request):
-    if request.user.is_authenticated:
-        return render(request, 'startpage/profile.html')
-    else:
-        return redirect('/')
+    return render(request, 'startpage/profile.html')
 
-
-
-#промежуточная страница при создании теста
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def create_test1(request):
 	args = {}
 	args.update(csrf(request))
 	args['form'] = TestCreationForm(instance=request.user)
-
-
 
 	if request.method == 'POST':
 		form = TestCreationForm(request.POST, instance=request.user)
@@ -50,7 +36,7 @@ def create_test1(request):
 			test.questions_number = form.cleaned_data['questions_number']
 			test.user_creator = request.user
 			test.save()
-			return redirect('/create_test/{0}/question_creation/{1}'.format(test.id, 1))
+			return HttpResponseRedirect(reverse('question_creation',args=[test.id, 1]))
 		else:
 			args['form'] = form
 			args['errors'] = form.errors
@@ -59,7 +45,7 @@ def create_test1(request):
 	return render(request,'startpage/test_creation_first.html', args)
 
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def question_creation(request, testid, questioncount):
 	global QUESTIONCOUNTER, QUESTIONCOUNT
 	args = {}
@@ -75,16 +61,16 @@ def question_creation(request, testid, questioncount):
 		if form.is_valid():
 			form.save()
 			if questions_number != questioncount:
-				return redirect('/create_test/{0}/question_creation/{1}'.format(testid,questioncount+1))
+				return HttpResponseRedirect(reverse('question_creation', args=[testid, questioncount+1]))
 			else:
-				return redirect('/create_test/{0}/success/'.format(testid))
+				return HttpResponseRedirect(reverse('test_created_successfully', args=[testid]))
 		else:
 			args['form'] = form
 			args['errors'] = form.errors
 
 	return render(request,'startpage/question_creation.html', args)
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def test_created_successfully(request, testid):
 	args = {}
 	test = Test.objects.get(id=testid)
@@ -94,7 +80,7 @@ def test_created_successfully(request, testid):
 	return render(request, 'startpage/test_created_successfully.html', args)
 
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def my_created_tests(request):
 
 	user = request.user
@@ -115,7 +101,7 @@ def my_created_tests(request):
 	print('CREATED_questions_number', created_questions_number)
 	return render(request, 'startpage/my_created_tests.html', args)
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def created_questions(request, testid):
 	args = {}
 	args['test'] = Test.objects.get(id=testid)
@@ -124,7 +110,7 @@ def created_questions(request, testid):
 	return render(request, 'startpage/created_questions.html', args)
 
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def tests_page(request):
 
 	tests = Test.objects.all()
@@ -132,7 +118,7 @@ def tests_page(request):
 	content['tests'] =  tests
 	return render(request, 'startpage/tests_page.html', content)
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def all_my_attempts(request, testid):
 	test = Test.objects.get(id=testid)
 	mytests = MyTest.objects.all().filter(test_id = test.id)
@@ -141,7 +127,7 @@ def all_my_attempts(request, testid):
 	content['mytests'] = mytests
 	return render(request, 'startpage/all_my_attempts.html', content)
 
-@login_required(login_url='/')
+@login_required(login_url=reverse_lazy('index'))
 def test_result(request, testid, mytestid):
 
 	mytest = MyTest.objects.get(id=mytestid)
@@ -153,29 +139,9 @@ def test_result(request, testid, mytestid):
 	for qr in qrs:
 		print('qr.question.answer_set', qr.question)
 
-
-	# print('My test id', mytest.id)
-	# content['questions'] = mytest.test.questions
-	# print('mytest.test', mytest.test)
-	# print('mytest.test.id', mytest.test.id)
-	# print('mytest.test.questions', mytest.test.questions)
-	# print('content[questions]', content['questions'])
-
-
 	return render(request, 'startpage/test_result.html', content)
-	# try:
-	# 	mytest =  MyTest.objects.get(id=mytestid)
-	# 	content = {}
-	# 	content['mytest'] = mytest
-	# 	content['questions'] = mytest.test.questions
-	# 	return render(request, 'startpage/test_result.html', content)
-	# except:
-	# 	print('Нет такого')
-	# 	return HttpResponseNotFound('<h1>Page not found</h1>')
-
 
 def login(request):
-
 	if request.user.is_authenticated:
 		return render(request,'startpage/profile.html')
 	else:
@@ -187,30 +153,17 @@ def login(request):
 			user = auth.authenticate(username=username, password=password)
 			if user is not None:
 				auth.login(request, user)
-				return redirect('/profile')
+				return HttpResponseRedirect(reverse('profile'))
 			else:
 				args['login_error'] = "User is not found"
 				return render_to_response('startpage/index.html', args)
 		else:
 			return render_to_response('startpage/index.html', args)
-	# args = {}
-	# args.update(csrf(request))
-	# if request.POST:
-	# 	username = request.POST.get('username', '')
-	# 	password = request.POST.get('password', '')
-	# 	user = auth.authenticate(username=username, password=password)
-	# 	if user is not None:
-	# 		auth.login(request, user)
-	# 		return redirect('/profile')
-	# 	else:
-	# 		args['login_error'] = "User is not found"
-	# 		return render_to_response('startpage/index.html', args)
-	# else:
-	# 	return render_to_response('startpage/index.html', args)
+
 
 def logout(request):
     auth.logout(request)
-    return redirect('/')
+    return HttpResponseRedirect(reverse('index'))
 
 def sign_up(request):
     args = {}
@@ -218,26 +171,18 @@ def sign_up(request):
     args['form'] = RegistrationForm()
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-
         if form.is_valid():
             form.save()
-            return redirect('/succes_signup')
-
+            return HttpResponseRedirect(reverse('succes_signup'))
     return render(request, 'startpage/signup.html', args)
-
 
 def account_created_successfully(request):
     return render(request, 'startpage/success_signup.html')
 
 
 def start_test_page(request, testid):
-
 	test = Test.objects.get(id=testid)
 	return render(request, 'startpage/start_test.html', {'test':test})
-
-
-
-
 
 current_lvl = 0
 wrong_count = 0
